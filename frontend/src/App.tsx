7149ae91564f6db7e946327866b1232e240dbcd4
Julia Olshanska <yulyaolshanska@gmail.com>
2025-05-16 20:24:33 +0300

    feat: add router to any path

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import { Suspense, lazy } from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 
 import Loader from './components/Loader/Loader';
 
@@ -12,6 +12,7 @@ const App: React.FC = () => {
       <Routes>
         <Route path="/" element={<RecipeListPage />} />
         <Route path="/recipes/:id" element={<RecipeDetailsPage />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </Suspense>
   );
